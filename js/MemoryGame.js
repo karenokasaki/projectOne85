0e88bb61661e5658e5444d6d32393ab8b86f6520
karen okasaki <oks.karen@gmail.com>
2022-08-17 12:02:18 -0300

    setting up project

---
+++ js/MemoryGame.js
@@ -0,0 +1,18 @@
+// construir minha classe
+
+class MemoryGame {
+    constructor() {
+
+        this.userName = ""
+        this.points = 10
+        this.deck = [
+            "./assets/harmonia.svg",
+            "./assets/poder.svg",
+            "./assets/projetar.svg",
+            "./assets/refletir.svg",
+        ]
+    }
+
+
+    
+}
