cdad7b2da81d97397d355477e69db273ab5da0c4
karen okasaki <oks.karen@gmail.com>
2022-08-17 14:41:10 -0300

    create img tag and start menu

--- js/MemoryGame.js
+++ js/MemoryGame.js
@@ -1,18 +1,41 @@
 // construir minha classe
 
 class MemoryGame {
-    constructor() {
+  constructor() {
+    this.userName = ""; //vai vir do inputName
+    this.points = 10; //estado de derrota quando points <= 0
+    this.deck = [
+      "./assets/harmonia.svg",
+      "./assets/poder.svg",
+      "./assets/projetar.svg",
+      "./assets/refletir.svg",
+      "./assets/harmonia.svg",
+      "./assets/poder.svg",
+      "./assets/projetar.svg",
+      "./assets/refletir.svg",
+    ];
+  }
 
-        this.userName = ""
-        this.points = 10
-        this.deck = [
-            "./assets/harmonia.svg",
-            "./assets/poder.svg",
-            "./assets/projetar.svg",
-            "./assets/refletir.svg",
-        ]
-    }
+  renderDeck() {
+    //randomizar a array this.deck e entregar ela pro forEach já randomizada.
+    let deckRandom = this.deck.sort(() => {
+      return Math.random() - 0.5;
+    });
 
+    //capturando o meu board
+    let board = document.querySelector("#board");
 
-    
+    //iterar pelo meu deck
+    deckRandom.forEach((imgSrc) => {
+      let imgTag = document.createElement("img"); // <img />
+      imgTag.src = imgSrc; // <img src="./assets/algumacoisa.svg" /> FRONT - CARD
+      imgTag.classList.add("hide"); // <img src="./assets/algumacoisa.svg" class="hide" />
+
+      let backImg = document.createElement("img"); // <img />
+      backImg.src = "./assets/fe.svg";
+
+      board.appendChild(imgTag);
+      board.appendChild(backImg);
+    });
+  }
 }
--- js/index.js
+++ js/index.js
@@ -1,1 +1,20 @@
 //manipulo o dom
+
+//instanciando nossa classe MemoryGame
+const game = new MemoryGame();
+
+const buttonStart = document.querySelector("#inicio button");
+const inicio = document.querySelector("#inicio");
+const inputName = document.querySelector("#inicio input");
+const name = document.querySelector("#name");
+const points = document.querySelector("#points");
+
+buttonStart.addEventListener("click", () => {
+  game.renderDeck();
+  inicio.style.display = "none";
+
+  game.userName = inputName.value;
+
+  name.innerText = game.userName;
+  points.innerText = game.points;
+});
